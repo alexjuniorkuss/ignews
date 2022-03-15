070130fb65a560ae8171bc7e7f3df7cdc6ff2339
Alex Junior Kuss <alex.kuss@havan.com.br>
2022-03-15 17:56:12 -0300

    quase

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,3 +1,5 @@
+import { signIn, useSession } from 'next-auth/react';
+import { api } from '../../services/api';
 import styles from './styles.module.scss';
 
 interface SubscribeButtonProps{
@@ -5,10 +7,27 @@ interface SubscribeButtonProps{
 }
 
 export function SubscribeButton({ priceId } : SubscribeButtonProps) {
+    const { data: session } = useSession()
+
+    async function handleSubscribe() {
+        if(!session) {
+            signIn('github')
+            return;
+        }
+
+        try{
+            const response = await api.post('/subscribe')
+
+            const { sessionId } = response.data;
+        } catch{
+            
+        }
+    }
     return (
         <button
             type="button"
             className={styles.subscribeButton}
+            onClick={handleSubscribe}
         >
             Subscribe now
         </button>
